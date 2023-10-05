3d96ddf61e7c4faf0acf21a875519d046bbf7939
Zacharie kouemegni <zachariekouemegni@zacharies-mbp.home>
2023-10-05 09:53:13 -0400

    add role

--- src/main/java/com/auth/Security/adm/AdminController.java
+++ src/main/java/com/auth/Security/adm/AdminController.java
@@ -4,6 +4,7 @@ import com.auth.Security.auth.AuthenticationService;
 import com.auth.Security.auth.UserResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -11,21 +12,26 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/v1/admin")
 @RequiredArgsConstructor
+@PreAuthorize("hasRole('ADMIN')")
 public class AdminController {
     private final AuthenticationService authenticationService;
     @GetMapping("/users")
+    @PreAuthorize("hasAuthority('admin:read')")
     public ResponseEntity<List<UserResponse>> getUsers(){
         return  ResponseEntity.ok(authenticationService.getUsers());
     }
     @PostMapping
+    @PreAuthorize("hasAuthority('admin:create')")
     public String post() {
         return "POST:: admin controller";
     }
     @PutMapping
+    @PreAuthorize("hasAuthority('admin:update')")
     public String put() {
         return "PUT:: admin controller";
     }
     @DeleteMapping
+    @PreAuthorize("hasAuthority('admin:delete')")
     public String delete() {
         return "DELETE:: admin controller";
     }
